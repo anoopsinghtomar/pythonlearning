4e27489006f75c8c228923eec01c451c613c34d9
Anoop Singh <anoopsingh1387@gmail.com>
2025-06-23 03:02:46 +0530

    first python commit

---
+++ seleniumscript/seleniumtest.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver= webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.python.org/")
+driver.find_element(by=By.XPATH, value='//*[@id="about"]/a')
+driver.quit()
+
