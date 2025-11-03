0f14d59612b621bc0e71e3678b43c19b954e0ca0
Anoop Singh Tomar <anoopsingh1387@gmail.com>
2025-11-03 09:53:41 +0530

    updated python commit - 3rd Nov

--- seleniumscript/seleniumtest.py
+++ seleniumscript/seleniumtest.py
@@ -5,5 +5,7 @@ driver= webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.python.org/")
 driver.find_element(by=By.XPATH, value='//*[@id="about"]/a')
-driver.quit()
+driver.maximize_window()
+driver.find_element(by=By.XPATH, value='//*[@id="documentation"]/a').click()
 
+# driver.close()
